78e56c1c7d9c9cb0cd60f29698be00f31651137c
Jonathan Treffler <mail@jonathan-treffler.de>
2020-12-04 14:41:38 +0000

    continued the development

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ console.log(canvas2D);
 var persons = [];
 
 let personCount = 20;
+let initalialInfected = 5;
+
+const circleRadius = 25;
 
 class Person {
     constructor(x, y, infected) {
@@ -18,9 +21,15 @@ class Person {
     }
     display(context) {
         context.beginPath();
-        context.fillStyle = 'green';
+
+        if(this.infected) {
+            context.fillStyle = 'red';
+        }else {
+            context.fillStyle = 'green';
+        }
+        
         context.lineWidth = 0;
-        context.arc(this.x, this.y, 20, 0, 2 * Math.PI, false);
+        context.arc(this.x, this.y, circleRadius, 0, 2 * Math.PI, false);
         context.fill();
         context.stroke();
     }
@@ -28,10 +37,44 @@ class Person {
         this.x += this.speedX;
         this.y += this.speedY;
     }
+    checkWalls() {
+        if(this.x < circleRadius || this.x + circleRadius > drawingCanvas.width) {
+            this.speedX *= -1;
+        }
+        if(this.y < circleRadius || this.y + circleRadius > drawingCanvas.height) {
+            this.speedY *= -1;
+        }
+    }
 }
 
-for(let i = 0; i < personCount; i++) {
-    persons.push(new Person(400, 400));
+function random(val1, val2) {
+    if(val2) {
+        var min = Math.ceil(val1);
+        var max = Math.floor(val2);
+    }else {
+        var min = 0;
+        var max = Math.floor(val1);
+    }
+
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomCoordinates() {
+    return {
+        x: random(circleRadius, drawingCanvas.width - circleRadius),
+        y: random(circleRadius, drawingCanvas.height - circleRadius)
+    };
+}
+
+for(let i = 0; i < personCount - initalialInfected; i++) {
+    let coordinates = randomCoordinates();
+    console.log(coordinates);
+    persons.push(new Person(coordinates.x, coordinates.y, false));
+}
+
+for(let i = 0; i < initalialInfected; i++) {
+    let coordinates = randomCoordinates();
+    persons.push(new Person(coordinates.x, coordinates.y, true));
 }
 
 // Draw Loop
@@ -41,6 +84,8 @@ setInterval(function() {
 
     for(person of persons) {
         person.move();
+        person.checkWalls();
+
         person.display(canvas2D);
     }
-}, 100);
+}, 10);
